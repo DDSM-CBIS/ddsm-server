0ee70f1090c975538d7c7cd2f3a66787c8e20bc5
yuvili <yuvalsh97@gmail.com>
2024-06-10 12:41:03 +0300

    feat(TAS-42): Get images by patient Id

--- App/__init__.py
+++ App/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from .filter_api.routes import filter_bp
-from .photos_api.routes import photos_bp
+from .images_api.routes import images_bp
 from .patients_api.routes import patients_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(filter_bp, url_prefix='/filter')
     app.register_blueprint(patients_bp, url_prefix='/patients')
-    app.register_blueprint(photos_bp, url_prefix='/photos')
+    app.register_blueprint(images_bp, url_prefix='/images')
     return app
--- App/images_api/utils.py
+++ App/images_api/utils.py
@@ -1,8 +1,9 @@
-import shutil
-
+from pathlib import Path
 from data.data import series_df
 import os
 
+def get_patient_image_folder(patientId):
+   return os.path.join(os.path.dirname(__file__), '..', '..', 'cache', f'{patientId}')
 
 def delete_files_in_folder(folder_path):
     for root, dirs, files in os.walk(folder_path):
@@ -11,17 +12,17 @@ def delete_files_in_folder(folder_path):
             os.remove(file_path)
 
 
-def get_photos_uids(patient_id):
-    photos_uids = list(series_df[series_df['PatientID']
+def get_image_uids(patient_id):
+    return list(series_df[series_df['PatientID']
                        .str
                        .contains(patient_id)]
                        .SeriesInstanceUID)
-    return photos_uids
 
 
-def create_if_not_exist_folder(folder_location: str):
-    if not os.path.isdir(folder_location):
-        os.mkdir(folder_location)
+def create_if_not_exist_folder(file_path: str):
+    if not os.path.exists(file_path):
+        path = Path(file_path)
+        path.mkdir(parents=True)
 
 def get_all_files_under_dir(directory_path):
     files_ret = []
@@ -30,36 +31,25 @@ def get_all_files_under_dir(directory_path):
         for file in files:
             if file == 'LICENSE':
                 continue
-            # Get the full path of the file and its extension
             file_path = os.path.join(root, file)
-            # Get the name of the directory containing the file
             dir_name = os.path.basename(root)
-            # get series name from dir name
             PatientID = series_df[series_df.SeriesInstanceUID == dir_name]['PatientID'].iat[0]
-            # Create the new name for the file
             new_file_name = f"{PatientID}_{file[2:]}"
 
-            # Construct the new path with the new file name
             new_file_path = os.path.join(root, new_file_name)
             if os.path.isfile(new_file_path):
                 continue
-            # Rename the file and move it to the same directory
             os.rename(file_path, new_file_path)
             files_ret.append(new_file_path)
     return files_ret
 
 
 def save_client_photos_in_cache(dst_path):
-    # collect all the pictures locations
     photos_paths = get_all_files_under_dir('tciaDownload/')
-    # moving them to cache
+    
     for src_path in photos_paths:
         file_name = os.path.basename(src_path)
         cur_dst_path = dst_path + '/' + file_name
         if not os.path.isfile(cur_dst_path):
             os.rename(src_path, cur_dst_path)
     delete_files_in_folder('tciaDownload/')
-
-
-def get_photo_urls(patient_id, dst_path):
-    return [f"http://127.0.0.1:5000/photos/{patient_id}/{filename}" for filename in os.listdir(dst_path)]
