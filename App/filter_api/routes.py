8a61cabac7de6b772eea82b4a3396c892808921a
Yuval Shabat <77205478+yuvili@users.noreply.github.com>
2024-06-10 12:49:03 +0300

    Tas 23 patient details (#3)
    * feat(TAS-23): Get patients details
    
    * feat(TAS-23): Get patient details by patient ID

--- App/filter_api/routes.py
+++ App/filter_api/routes.py
@@ -1,47 +1,13 @@
 import json
-from flask import Blueprint, request, jsonify
-from data.data import calc_df, mass_df, pd
-from data.data import extract_unique_values
+from flask import Blueprint, jsonify
+from data.utils import get_common_columns, extract_unique_values, get_distinct_columns
 filter_bp = Blueprint('filter', __name__)
-
-
-def filter_by(df: pd.DataFrame, filters: dict):
-    for col, value in filters.items():
-        if col in df.columns:
-            if col == 'patient_id':
-                df = df.loc[df[col].str.contains(value)]
-            else:
-                df = df.loc[df[col] == value]
-    return df
-
-
-@filter_bp.route('/', methods=['GET'])
-def filtering():
-    # get the filters as json from the user in an API request
-    filters = request.json
-    # filter the calc data
-    calc_by_filters = set(filter_by(calc_df, filters)['patient_id'])
-    # filter the mass data
-    mass_by_filters = set(filter_by(mass_df, filters)['patient_id'])
-    # unite all the patient_id that conform to the filters
-    patients = mass_by_filters.union(calc_by_filters)
-    # return list of
-    return jsonify(list(patients))
 
 @filter_bp.route('/options', methods=['GET'])
 def filter_options():
-    print("filter options")
-    columns_to_extract = [
-        'leftOrRightBreast',
-        'imageView',
-        'abnormalityId',
-        'abnormalityType',
-        'breastDensity',
-        'subtlety',
-        'assessment',
-        'pathology'
-    ]
+    columns_to_extract = get_common_columns()
     response = extract_unique_values(columns_to_extract)
+    
     real = {}
     for key in response:
         real[key] = response[key]
@@ -51,18 +17,18 @@ def filter_options():
 
 @filter_bp.route('/abnormality-options', methods=['GET'])
 def filter_options_mass():
-    columns_to_extract = [
-        'calcType',
-        'calcDistribution',
-        'massShape',
-        'massMargins',
-    ]
+    columns_to_extract = get_distinct_columns()
     response = extract_unique_values(columns_to_extract)
     real = {}
     for key in response:
         real[key] = response[key]
 
-        
     realJson = json.dumps(real, default=str)
+    return jsonify(realJson), 200
+
+@filter_bp.route('/patients-ids', methods=['GET'])
+def filter_options_patients():
+    response = extract_unique_values(['patientId'])
+    realJson = json.dumps(response, default=str)
     return jsonify(realJson), 200
 
--- App/patients_api/routes.py
+++ App/patients_api/routes.py
@@ -1,34 +1,31 @@
 import json
 import pandas as pd
 from flask import Blueprint, jsonify
-from data.data import calc_df, mass_df, extract_unique_values
+from data.utils import calcDf, massDf
 patients_bp = Blueprint('patients', __name__)
-
-
-def filter_by(df: pd.DataFrame, filters: dict):
-    for col, value in filters.items():
-        if col in df.columns:
-            if col == 'patient_id':
-                df = df.loc[df[col].str.contains(value)]
-            else:
-                df = df.loc[df[col] == value]
-    return df
 
 @patients_bp.route('/', methods=['GET'])
 def patients():
-    merged = pd.merge(calc_df, mass_df, how='outer')
-    result_dict = {}
+    patients = {}
+
+    for df in [calcDf, massDf]:
+        for index, row in df.iterrows():
+            patient_id = row['patientId']
+            if patient_id not in patients:
+                patients[patient_id] = []
+            patients[patient_id].append(row.dropna().drop('patientId').to_dict())
     
-    grouped = merged.groupby('patientId')
-    for patient_id, group in grouped:
-        rows_list = group.drop(columns='patientId').to_dict(orient='records')
-        result_dict[patient_id] = rows_list
-    
-    realJson = json.dumps(result_dict, default=str)
+    realJson = json.dumps(patients, default=str)
     return jsonify(realJson), 200
 
-@patients_bp.route('/ids', methods=['GET'])
-def patient_ids():
-    response = extract_unique_values(['patientId'])
-    realJson = json.dumps(response, default=str)
+@patients_bp.route('/<patientId>', methods=['GET'])
+def patient(patientId):
+    patient= []
+
+    for df in [calcDf, massDf]:
+        for index, row in df.iterrows():
+            if row['patientId'] == patientId:
+                patient.append(row.dropna().drop('patientId').to_dict())
+    
+    realJson = json.dumps(patient, default=str)
     return jsonify(realJson), 200
